b17053b4d7bb5f167a52d46d35464d5991ec9ac6
KhurramSagoo <uaf.khurram@gmail.com>
2023-06-20 15:00:15 +0500

    deploy-start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Header from './Component/Header';
+import MainBody from './Component/MainBody';
+
+function App() {
+  return (
+    <div className='container'>
+    <Header/>
+    <MainBody />
+    
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+import Logo from './img/logo.png'
+
+const Header = () => {
+  return (
+    <nav className='header'>
+<img src={Logo} alt="Logo" />
+<h3>React Facts</h3>
+<h4>React Course - Project 1</h4>
+    </nav>
+  )
+}
+
+export default Header
