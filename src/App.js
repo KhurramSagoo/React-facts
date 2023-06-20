c828e4f33f528c535559da111891a980febb9bb6
KhurramSagoo <uaf.khurram@gmail.com>
2023-06-20 15:26:53 +0500

    try again deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.scss';
-import Header from './Component/Header';
-import MainBody from './Component/MainBody';
+import "./App.scss";
+import Header from "./Component/Header";
+import MainBody from "./Component/MainBody";
 
 function App() {
   return (
-    <div className='container'>
-    <Header/>
-    <MainBody />
-    
-    
+    <div className="container">
+      <Header />
+      <MainBody />
     </div>
   );
 }
--- src/Component/MainBody.jsx
+++ src/Component/MainBody.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import './style.scss'
-import BigLogo from './img/logo-large.png'
+import React from "react";
+import "./style.scss";
+import BigLogo from "./img/logo-large.png";
 
 const MainBody = () => {
   return (
-    <div className='main'>
-    <h1>Fun facts about React</h1>
-    <ul>
+    <div className="main">
+      <h1>Fun facts about React</h1>
+      <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-    <img src={BigLogo} alt="Big-React" className='logo-large' />
-    
-    
+      </ul>
+      <img src={BigLogo} alt="Big-React" className="logo-large" />
     </div>
-  )
-}
+  );
+};
 
-export default MainBody
+export default MainBody;
